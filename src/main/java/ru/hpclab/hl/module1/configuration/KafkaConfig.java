f4d3faa2847a581fdf9c0ba60116b509ffb110a2
Irina <iravas981@icloud.com>
2025-05-01 23:33:22 +0300

    LAB

--- src/main/java/ru/hpclab/hl/module1/configuration/KafkaConfig.java
+++ src/main/java/ru/hpclab/hl/module1/configuration/KafkaConfig.java
@@ -1,5 +1,6 @@
 package ru.hpclab.hl.module1.configuration;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +18,7 @@ import java.util.Map;
 
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConfig {
     @Value("${kafka.hosts:10.60.3.27:9094,10.60.3.28:9094}")
     private List<String> bootstrapAddresses;
 
@@ -51,4 +53,12 @@ public class KafkaConsumerConfig {
         factory.setBatchListener(true);
         return factory;
     }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddresses);
+        return new KafkaAdmin(configs);
+    }
+
 }
